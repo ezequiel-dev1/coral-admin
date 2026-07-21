9d2011d5079784b0b0da592b9f20100b312adc21
ezequiel.dev1 <ezequiel.dev1@outlook.com>
2026-07-21 14:54:30 -0700

    Suppress hydration warning from browser extensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className={cn("font-sans", geist.variable)} suppressHydrationWarning>
       <body>
         {children}
       </body>
